5f6c629bc02cfe903c6a9b9254d769d02731f6dc
linto-antony <lintoantonyv@gmail.com>
2026-09-04 15:52:21 +0530

    [MIG] web_widget_mpld3_chart: Migration to 19.0

--- web_widget_mpld3_chart/models/abstract_mpld3_parser.py
+++ web_widget_mpld3_chart/models/abstract_mpld3_parser.py
@@ -11,7 +11,7 @@ _logger = logging.getLogger(__name__)
 try:
     import mpld3
     from bs4 import BeautifulSoup
-except (OSError, ImportError) as err:
+except (OSError, ImportError) as err:  # pragma: no cover
     _logger.debug(err)
 
 
@@ -20,7 +20,7 @@ class AbstractMpld3Parser(models.AbstractModel):
     _description = "Utility to parse ploot figure to json data for widget Mpld3"
 
     @api.model
-    def convert_figure_to_json(self, figure):
+    def convert_figure_to_json(self, figure):  # pragma: no cover
         html_string = mpld3.fig_to_html(figure, no_extras=True, include_libraries=False)
         soup = BeautifulSoup(html_string, "lxml")
         json_data = {
